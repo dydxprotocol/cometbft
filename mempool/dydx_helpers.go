dydxprotocol/tendermint

add dydx-specific changes to v1 mempool

mempool/dydx_helpers.go
@@ -1,0 +1,29 @@
+package mempool
+
+import (
+	cosmostx "github.com/cosmos/cosmos-sdk/types/tx"
+	"github.com/tendermint/tendermint/libs/log"
+	"github.com/tendermint/tendermint/types"
+)
+
+// isClobOrderTransaction returns true if the provided `tx` is a
+// Cosmos transaction containing a `MsgPlaceOrder` or `MsgCancelOrder` message.
+func IsClobOrderTransaction(
+	tx types.Tx,
+	mempoolLogger log.Logger,
+) bool {
+	cosmosTx := &cosmostx.Tx{}
+	err := cosmosTx.Unmarshal(tx)
+	if err != nil {
+		mempoolLogger.Error("isClobOrderTransaction error. Invalid Cosmos Transaction.")
+		return false
+	}
+
+	if len(cosmosTx.Body.Messages) == 1 &&
+		(cosmosTx.Body.Messages[0].TypeUrl == "/dydxprotocol.clob.MsgPlaceOrder" ||
+			cosmosTx.Body.Messages[0].TypeUrl == "/dydxprotocol.clob.MsgCancelOrder") {
+		return true
+	}
+
+	return false
+}

dydxprotocol/cometbft

mempool/dydx_helpers.go
@@ -1,0 +1,29 @@
+package mempool
+
+import (
+	"github.com/cometbft/cometbft/libs/log"
+	"github.com/cometbft/cometbft/types"
+	cosmostx "github.com/cosmos/cosmos-sdk/types/tx"
+)
+
+// isClobOrderTransaction returns true if the provided `tx` is a
+// Cosmos transaction containing a `MsgPlaceOrder` or `MsgCancelOrder` message.
+func IsClobOrderTransaction(
+	tx types.Tx,
+	mempoolLogger log.Logger,
+) bool {
+	cosmosTx := &cosmostx.Tx{}
+	err := cosmosTx.Unmarshal(tx)
+	if err != nil {
+		mempoolLogger.Error("isClobOrderTransaction error. Invalid Cosmos Transaction.")
+		return false
+	}
+
+	if len(cosmosTx.Body.Messages) == 1 &&
+		(cosmosTx.Body.Messages[0].TypeUrl == "/dydxprotocol.clob.MsgPlaceOrder" ||
+			cosmosTx.Body.Messages[0].TypeUrl == "/dydxprotocol.clob.MsgCancelOrder") {
+		return true
+	}
+
+	return false
+}
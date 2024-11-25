5ef6722fecc6ef7b8dceeb1ca6d55fa69e001bfc
Teddy Ding <teddy@dydx.exchange>
2024-11-25 13:25:53 -0500

    change test

--- test/e2e/app/app.go
+++ test/e2e/app/app.go
@@ -277,7 +277,7 @@ func (app *Application) FinalizeBlock(_ context.Context, req *abci.RequestFinali
 				},
 			},
 		},
-		NextBlockDelay: 1 * time.Second,
+		NextBlockDelay: 0, // default back to using `timeout_commit` in tests.
 	}, nil
 }
 
